27b44dd4bb7d5a0ac7f54676efc8c1ddadc9d0e1
Bojorge <38023924+Bojorge@users.noreply.github.com>
2021-06-03 20:49:27 -0600

    Creación de las API

---
+++ API/Relacional SQL SERVER/SQLServerAPI/SQLServerAPI/Controllers/xController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQLServerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class xController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            return "es correcto mi estimado";
+
+        }
+
+        [HttpGet("{h}")]
+        public string Get(int h)
+        {
+            return h switch
+            {
+                1 => "Esveme",
+                2 => "ak7",
+                _ => throw new NotSupportedException("el numero no es valido")
+            };
+
+        }
+
+        public string Post(humano saico)
+        {
+            return saico.nombre + " >>> lo volviste a lograr lml";
+        }
+    }
+
+
+    public class humano
+    {
+        public string cerebros { get; set; }
+        public string nombre { get; set; }
+    }
+}
